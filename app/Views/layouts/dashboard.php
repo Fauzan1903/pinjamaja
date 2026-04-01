5d1795b11a72bca46c508af02b7994f001e9d08b
fauzan1903 <sundara1903@gmail.com>
2026-04-01 10:57:59 +0700

    README.md

---
+++ app/Views/layouts/dashboard.php
@@ -0,0 +1,8 @@
+<?= $this->extend('layouts/main') ?>
+<?= $this->section('content') ?>
+<div class="container mt-2">
+    <p>Ini adalah Halaman Dashboard
+        <br>Selamat datang di <b>Pinjamin Aja</b>, aplikasi yang memudahkan Anda dalam mengelola barang pinjaman dengan cepat dan efisien.
+    </p>
+</div>
+<?= $this->endSection() ?>
